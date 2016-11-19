c8ccb0cf3bda22c13032a13896c1b0e6a77775b9
gam3boy1 <aadianand1234@gmail.com>
2016-11-19 10:30:28 +0800

    hi

--- PhaserGame/app.js
+++ PhaserGame/app.js
@@ -38,7 +38,7 @@ function create() {
     player = game.add.sprite(32, game.world.height - 150, 'dude');
     //animate the sprite
     player.animations.add('left', [0,1,2,3], 10, true);
-    player.animations.add('right', [5,6,7,8, 10, true);
+    player.animations.add('right', [5,6,7,8], 10, true);
     game.physics.arcade.enable(player);
     //Physics properties
     player.body.bounce.y = 0.2;
