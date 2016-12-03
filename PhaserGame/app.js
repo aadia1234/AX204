3beb4f9f5ddc043344a808b4791ec22511ffa306
gam3boy1 <aadianand1234@gmail.com>
2016-12-03 10:22:20 +0800

    sui

--- PhaserGame/app.js
+++ PhaserGame/app.js
@@ -35,7 +35,7 @@ function create() {
     ledge.body.immovable = true;
 
     //Player
-    player = game.add.sprite(32r, game.world.height - 150, 'dude');
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
     //animate the sprite
     player.animations.add('left', [0,1,2,3], 10, true);
     player.animations.add('right', [5,6,7,8], 10, true);
